2556dec75b4fd50cdefd64818a3b6504f6138c37
huriferenc <huri.ferenc@gmail.com>
2022-09-18 01:11:32 +0200

    *  Fixed non-deploy bug with renamed card-match-ci.yml *  Removed unnecessary tests

--- src/app/local-storage.spec.ts
+++
@@ -1,7 +0,0 @@
-import { LocalStorage } from './local-storage';
-
-describe('LocalStorage', () => {
-  it('should create an instance', () => {
-    expect(new LocalStorage()).toBeTruthy();
-  });
-});
